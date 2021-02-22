6bb34c11e8164bc34a72206832896c4d0d494526
JoyMbugua <wacumbugua@gmail.com>
2021-02-22 19:59:23 +0300

    added squad model and view

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,9 @@
+import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,6 +21,19 @@ public class App {
         get("/squad/form",(req, res) ->{
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "squad-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //showing all heroes
+        get("/squad",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+
+            ArrayList<Hero> members = Hero.getAll();
+            model.put("heroes",members);
+
+            return new ModelAndView(model, "squad.hbs");
         }, new HandlebarsTemplateEngine());
 
     }
