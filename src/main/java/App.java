8d8228e157a86e0cd80ecbd14fb3751a1d834a05
JoyMbugua <wacumbugua@gmail.com>
2021-02-22 16:58:44 +0300

    added root route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs")
+        })
+    }
 }
