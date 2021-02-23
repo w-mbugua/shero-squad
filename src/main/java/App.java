9e5b8c28ce2a171db5816eb5b44da5f91aa38bd8
JoyMbugua <wacumbugua@gmail.com>
2021-02-23 08:57:36 +0300

    added maven files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,15 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
+   static int getHerokuAssignedPort(){
+       ProcessBuilder processBuilder = new ProcessBuilder();
+       if(processBuilder.environment().get("PORT") != null){
+           return Integer.parseInt(processBuilder.environment().get("PORT"));
+       }
+       return 4567;
+   }
     public static void main(String[] args) {
+       port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         get("/", (request, response) -> {
